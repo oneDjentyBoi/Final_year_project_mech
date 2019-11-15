02cc32a53e371b0dba588f174b22ea911a346322
oneDjentyBoi <46261279+oneDjentyBoi@users.noreply.github.com>
2019-11-15 12:11:55 +0530

    Add files via upload

--- Cylindrer_TDMA.py
+++ Cylindrer_TDMA.py
@@ -3,54 +3,113 @@ All quantities are in SI units.
 This is the code for the temperature profile of a cylinder.
 Thermal conductivity is assumed to be constant here.
 Temp at r=0 > Temp at r=R
-e = sub-diagonal vector
-f = diagonal vector
-g = super_diagonal vector 
+b = sub-diagonal vector
+a = diagonal vector
+c = super_diagonal vector 
 """
 #############################################################
-Tr=300 # temp at r=R
-q_v=1000000 # heat gen
-k=100 # thermal conductivity
-r=0.04 # radius
-n=50 # number of divisions
-Tf = Tr + (q_v*r*r)/4 # temp at r=0; solved from analytical formula.
+Tr=100 # temp at r=R
+q_v=100 # heat gen
+k=164 # thermal conductivity
+r=4 # radius
+n=50 # number of nodes
 ##############################################################
+import numpy as np
+import pandas as pd
+def TDMAsolver(a, b, c, d):
+    nf = len(d) # number of equations
+    ac, bc, cc, dc = map(np.array, (a, b, c, d)) # copy arrays
+    for it in range(1, nf):
+        mc = ac[it-1]/bc[it-1]
+        bc[it] = bc[it] - mc*cc[it-1] 
+        dc[it] = dc[it] - mc*dc[it-1]
+        	    
+    xc = bc
+    xc[-1] = dc[-1]/bc[-1]
+    # print(bc)
+    # print(dc)
+    # print(cc)
+    for il in range(nf-2, -1, -1):
+        xc[il] = (dc[il]-cc[il]*xc[il+1])/bc[il]
+
+    return xc
 import matplotlib.pyplot as plt
-del_r=r/n 
-e=[0]*(n-2)
-f=[0]*(n-2)
-g=[0]*(n-3)
-x=[0]*(n-2)
-y=[0]*(n-2)
-y[0]=-((2*del_r-del_r)*Tr + (q_v/k)*2*del_r*del_r*del_r)
-y[n-3]=-((2*del_r*(n-2)+del_r)*Tf + (q_v/k)*2*del_r*(n-2)*del_r*del_r) 
-for i in range(1,n-3):
-	y[i] = -(q_v/k)*2*del_r*del_r*del_r*(i+1)
-for i in range(1,n-2):
-	e[i]=2*del_r*(i+1)-del_r
-for i in range(0,n-2):
-	f[i]=-4*del_r*(i+1)
-for i in range(0,n-3):
-	g[i]=2*del_r*(i+1) + del_r
-for i in range(1,n-2):
-	factor=e[i]/f[i-1]
-	f[i]=f[i]-factor*g[i-1]
-	y[i]=y[i]-factor*y[i-1]
-x[n-3]=y[n-3]/f[n-3]
-for i in range(n-4,-1,-1):
-	x[i] = (y[i] - g[i]*x[i+1])/f[i]
-# print(e)
-# print(f)
-# print(g)
-# print(y)
-# print(x)
-y1=[0]*n
-y1[0]=Tf
-y1[n-1]=Tr
-x1=[0]*n
-for i in range(n-3,-1,-1):
-	y1[n-i-2]=x[i]
-for i in range(1,n):
-	x1[i]= del_r*i
-plt.plot(x1,y1)
-plt.show()
+
+del_r=r/n
+
+m=n-1 #number of divisions.
+
+a=[]
+b=[]
+c=[]
+
+# b=np.array([0.004, 0.012, 0.02, 0.028, 0.036000000000000004, 0.044, 0.052000000000000005, 0.06])
+# a=np.array([1, -0.016, -0.032, -0.048, -0.064, -0.08, -0.096, -0.112, -0.06])
+# c=np.array([0, 0.012, 0.02, 0.028, 0.036000000000000004, 0.044, 0.052000000000000005, 0.06])
+# y=np.array([300, 0, 0, 0, 0, 0, 0, 0, 0])
+# t=TDMAsolver(b,a,c,y)
+# print(t)
+
+if q_v>0:
+	y=[0]*n
+	y[0]=Tr
+	a.append(1)
+	for i in range(1,n-1): 
+		a.append(-4*del_r*i)
+	a.append(-1)
+	for i in range(1,n-1):
+		b.append((2*del_r*i)-del_r)
+	b.append(1)
+	c.append(0)
+	for i in range(1,n-1):
+		c.append((2*del_r*i)+del_r)
+
+	y[n-1] = -q_v/(4*k)
+
+	for i in range(1,n-1):
+		y[i]=-(q_v*del_r*del_r*del_r*2*i)/k
+
+	t=TDMAsolver(b,a,c,y)
+	print(t)
+
+	r_x=[0]*n
+
+	for i in range(0,n):
+		r_x[i]=(n-i)*del_r
+	plt.plot(r_x,t)
+	plt.show()
+	_x=np.array(r_x)
+	_y=np.array(t)
+	_y=_y[::-1]
+	arr= {'x':_x,'y':_y}
+	df=pd.DataFrame(arr)
+	df.to_excel("plot.xlsx",index=True)
+else:
+	a.append(1)
+	for i in range(1,n-2):
+		a.append(-4*del_r*i)
+	a.append(-4*del_r*(m-1)+2*del_r*(m-1)+del_r)
+	for i in range(1,n-1):
+		b.append((2*del_r*i)-del_r)
+	c.append(0)
+	for i in range(1,n-2):
+		c.append((2*del_r*i)+del_r)
+
+	y=[0]*(n-1)
+	y[0]=Tr
+
+	t=TDMAsolver(b,a,c,y)
+	print(t)
+
+	r_x=[0]*(n-1)
+
+	for i in range(0,n-1):
+		r_x[i]=(n-1-i)*del_r
+	plt.plot(r_x,t)
+	plt.show()
+
+# print((a))
+# print((b))
+# print((c))
+# print((y))
+
